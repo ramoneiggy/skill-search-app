d5949a96c2ea18c85368e802842b4c2f9c42af5e
iggy <ramoneiggy@gmail.com>
2020-01-30 00:07:47 +0100

    Initial commit

---
+++ src/main/java/com/isolaja/skillsearchapp/SkillSearchApp.java
@@ -0,0 +1,13 @@
+package com.isolaja.skillsearchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSearchApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillSearchApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/isolaja/skillsearchapp/SkillSearchAppTests.java
@@ -0,0 +1,13 @@
+package com.isolaja.skillsearchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSearchAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
